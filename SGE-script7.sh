d26dd9a6a849f4588e3b5c171cb8a9fda4290443
Sara Gonzalez Estebanez <sargonest2@alu.edu.gva.es>
2024-10-04 12:06:37 +0200

    Update SGE-script7.sh

--- SGE-script7.sh
+++ SGE-script7.sh
@@ -4,7 +4,5 @@ echo -e "\n Ingresa la ruta del directorio a comprimir: \n"
 read ruta
 dir="${ruta##*/}"
 fecha=$(date +%Y%m%d_%H%M%S)
-tar -czf "backup_$fecha.tar.gz" "$ruta"
+tar -czf "backup_$fecha-$dir.tar.gz" "$ruta"
 echo "Backup de $dir completado: backup_$fecha-$dir.tar.gz"
-
-#echo -e "\nNo me sale, dejarlo para luego\n"
